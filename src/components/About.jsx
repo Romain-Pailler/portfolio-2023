460df7ca608019b65d5da86d5cff9cf42d1e7c64
Romain-Pailler <romain.pailler77@gmail.com>
2023-05-14 14:13:48 +0200

    Portfolio Romain 2023

---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>À propos</p>
+            </div>
+            <p className='text-xl mt-20'>
+            Bonjour !<br/> <br/>
+            Je m'appelle Romain, j'ai 22 ans et je suis passionné d'informatique depuis toujours. Mon intérêt pour la programmation m'a amené à poursuivre des études dans ce domaine. <br/>Je suis actuellement en BTS SIO options SLAM à l'école IPSSI à Marne-la-Vallée, où j'ai acquis des compétences solides en développement web et en programmation orientée objet. <br/> <br/>Avant cela, j'ai effectué deux ans de licence en Informatique générale à l'université de Paris où j'ai pu découvrir le monde de la programmation.<br/>Ensuite je compte effectuer par la suite un Bachelor Développement fullstack & DevOps au sein de cette école.<br/><br/>
+
+            Mes centres d'intérêt, en dehors de l'informatique, sont la course à pied et le cinéma. La course m'aide à me ressourcer et à me concentrer, tandis que le cinéma m'inspire et me divertit.
+            </p>
+            <br />
+            <p className='text-xl'>
+              <br/>
+            Mon objectif professionnel est de devenir développeur FullStack. J'apprécie particulièrement le défi de travailler sur des projets de grande envergure et de résoudre des problèmes complexes. <br/><br/>Ainsi ce portfolio est une vitrine de mes compétences en programmation et en développement web. J'y présente mes projets et mes réalisations académiques, dans l'espoir de trouver de nouvelles opportunités professionnelles.<br/>
+            </p>
+
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Experience.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import Angular from '../assets/angular.png';
+import C from '../assets/c.png';
+import CSS from '../assets/css.png';
+import Git from '../assets/git.png';
+import HTML from '../assets/html.png';
+import Java from '../assets/java.png';
+import JS from '../assets/javascript.png';
+import MySql from '../assets/mysql.png';
+import PHP from '../assets/php.png';
+import Python from '../assets/python.png';
+import react from '../assets/react.png';
+import Tailwind from '../assets/tailwind.png';
+const Experience = () => {
+    const techs = [
+        {
+            id: 1,
+            src: HTML,
+            title: 'HTML',
+            style: 'shadow-orange-500'
+        },
+        {
+            id: 2,
+            src: CSS,
+            title: 'CSS',
+            style: 'shadow-blue-500'
+        },
+        {
+            id: 3,
+            src: JS,
+            title: 'JavaScript',
+            style: 'shadow-yellow-500'
+        },
+        {
+            id: 4,
+            src: Tailwind,
+            title: 'Tailwind',
+            style: 'shadow-sky-400'
+        },
+        {
+            id: 5,
+            src: PHP,
+            title: 'Php',
+            style: 'shadow-blue-700'
+        },
+        {
+            id: 6,
+            src: Java,
+            title: 'Java',
+            style: 'shadow-red-500'
+        },
+        {
+            id: 7,
+            src: C,
+            title: 'C',
+            style: 'shadow-blue-500'
+        },
+        {
+            id: 8,
+            src: MySql,
+            title: 'MySql',
+            style: 'shadow-blue-500'
+        },
+        {
+            id: 9,
+            src: Git,
+            title: 'Git',
+            style: 'shadow-orange-500'
+        },
+        {
+            id: 10,
+            src: react,
+            title: 'React (Bientôt)',
+            style: 'shadow-blue-500'
+        },
+        {
+            id: 11,
+            src: Angular,
+            title: 'Angular (Bientôt)',
+            style: 'shadow-red-600'
+        },
+        {
+            id: 12,
+            src: Python,
+            title: 'Python (Bientôt)',
+            style: 'shadow-yellow-600'
+        }
+    ]
+  return (
+    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-full mt-0'>
+        <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
+            <div>
+                <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Expériences</p>
+                <p className='py-6'>Les languages que j'ai déjà utilisé :</p>
+            </div>
+
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+                {
+                    techs.map(({id, src, title, style})=> (
+                        <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                    <img src={src} alt="" className='w-20 mx-auto'/>
+                    <p className='mt-4'>{title}</p>
+                </div>
+                    ))
+                }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
